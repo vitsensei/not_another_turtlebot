228277c0d48b1e3abfb94ac7a4c2166d51902247
vitsensei <42131264+vitsensei@users.noreply.github.com>
2019-11-07 19:38:01 +1030

    Add files via upload
    Correctly find the pose of the rover. Implemented the control algorithm (without testing).

--- rover_project/src/rover_movement_controller.py
+++ rover_project/src/rover_movement_controller.py
@@ -3,7 +3,10 @@
 import rospy
 from gazebo_msgs.srv import GetModelState
 from std_msgs.msg import Float64
-import time
+
+import numpy as np
+
+integrated_heading_error = 0;
 
 def callService(rover_name):
 	# rover_name = "{model_name: " + rover_name + "}"
@@ -16,6 +19,38 @@ def callService(rover_name):
 	except rospy.ServiceException as e:
 		print("Service call failed: ",e)
 
+def heading_pid_controller(heading_error):
+	global integrated_heading_error
+
+	kp = 1
+	ki = 0.1
+	kd = 0
+
+	if (abs(integrated_heading_error) < 3.14):
+		integrated_heading_error = integrated_heading_error + heading_error
+
+	angular_vel = kp*heading_error + ki*integrated_heading_error
+
+	return angular_vel
+
+def control_vector_compute(current_point, desired_point, current_heading):
+	error_vector = desired_point - current_point
+
+	k = atan2(np.linalg.norm(error_vector)/5,1)
+	control_vector = k*error_vector
+
+	linear_vel = np.linalg.norm(control_vector)
+	desired_heading = atan(control_vector[2]/control_vector[1])
+
+	delta_heading = atan2(sin(desired_heading-current_heading)/cos(desired_heading-current_heading))
+
+	angular_vel = heading_pid_controller(delta_heading)
+
+	left_wheel_cmd = (2*linear_vel + angular_vel*1.1)/2
+	right_wheel_cmd = -(2*linear_vel - angular_vel*1.1)/2
+
+	return left_wheel_cmd, right_wheel_cmd
+
 def main():
 	rospy.init_node("movement_controller_node")
 	rate = rospy.Rate(1)
@@ -26,9 +61,13 @@ def main():
 
 	while not rospy.is_shutdown():
 		returned_state = callService("rover")
-		print("Returned state: ",returned_state)
+		# print("Returned state: ",returned_state)
+		print("Rover's pose:")
+		print("X: ", returned_state.pose.position.x)
+		print("Y: ", returned_state.pose.position.y)
+		print("Phi: ", returned_state.pose.orientation.z)
 
-		left_wheel_cmd = 1
+		left_wheel_cmd = 2
 		right_wheel_cmd = -1
 
 		left_wheel_pub.publish(left_wheel_cmd)
